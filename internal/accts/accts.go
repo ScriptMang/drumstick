b3a542ed12edc9f4338e985db11e3ccc37538cb5
Andy Peralta <aperalta686@gmail.com>
2025-02-05 16:58:17 -0500

    add functs for inserting user account information
    added functs that map an account struct fields to their respective
    tables in the drumstick database. It breaks apart the fields
    in the Account struct into two databse insertions for the user account
    and user profile struct.
    
    - added funct addUserAccount, this funct takes an *Account
      so it can insert the user account fields to the database and assigns
      the new id value to account struct id field. It returns an error if one
      exists otherwise its nil
    
      - added funct encryptPassword, to encrypt a user's Passwords.
        it takes a byte slice and is then encrypted using bcrypt.
        It returns a byte slice and an error
    
    - added helper funct UserIDBYUsername to get the user's id
      from user account table in the datbase
    
    - added funct addUserProfile which takes an account struct
      and inserts them into the database a row in the user profile table.
      It returns an error if it exists otherwise its nil.
    
    - added funct CreateAcct which performs the insertions for
      both user account and user profile tables. It takes as a parameter
      an account struct and returns an string and an error.
      If it an error exists, it also returns an empty string. If
      no error exists a returns a string message saying the specified
      user has been registered, and a nil error

--- internal/accts/accts.go
+++ internal/accts/accts.go
@@ -1,5 +1,16 @@
 package accts
 
+import (
+	"errors"
+	"fmt"
+	"scriptmang/drumstick/internal/backend"
+
+	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type UserProfile struct {
 	ID      int    `json:"id" form:"id"`
 	UserID  int    `json:"user_id" form:"user_id"`
@@ -34,4 +45,111 @@ type Posts struct {
 	NumbBookmarks int    `json:"number_bookmarks" form:"number_bookmarks"`
 }
 
+// encrypts a byte slice secret using bcrypt
+func encryptPassword(s []byte) ([]byte, error) {
+	hash, err := bcrypt.GenerateFromPassword(s, 14)
+	return hash, err
+}
+
+// add user account to the database
+func addUserAcct(acct *Account) error {
+	ctx, db := backend.Connect()
+	defer db.Close()
+
+	var err error
+	acct.Password, err = encryptPassword(acct.Password)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			fmt.Println(pgErr.Code)
+			fmt.Println(pgErr.Message)
+		}
+
+		if errors.Is(err, bcrypt.ErrPasswordTooLong) {
+			fmt.Println(err)
+		}
+
+		if errors.Is(err, bcrypt.ErrHashTooShort) {
+			fmt.Println(err)
+		}
+
+		return fmt.Errorf("error: %s, code: %s", pgErr.Message, pgErr.Code)
+	}
+
+	var tempID int
+	err = db.QueryRow(ctx,
+		`INSERT INTO user_account(username, password) VALUES($1, $2) RETURNING id`,
+		acct.Username, acct.Password,
+	).Scan(&tempID)
+
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			fmt.Println(pgErr.Code)
+			fmt.Println(pgErr.Message)
+		}
+		return fmt.Errorf("error: %s, code: %s", pgErr.Message, pgErr.Code)
+	}
+
+	acct.ID = tempID
+	return nil
+}
+
+// get the user's id from their username
+func UserIDByUsername(username string) (int, error) {
+	ctx, db := backend.Connect()
+	defer db.Close()
+
+	var possibleUserID []*int
+	err := pgxscan.Select(ctx, db, &possibleUserID, `SELECT id FROM user_account WHERE username = $1`, username)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return 0, fmt.Errorf("error: resource not found: id does not exist")
+		}
+		return 0, fmt.Errorf("error: %v", err)
+	}
+	userID := *possibleUserID[0]
+	return userID, nil
+}
+
+// add user account to the database
+func addUserProfile(acct Account) error {
+	ctx, db := backend.Connect()
+	defer db.Close()
+
+	_, err := db.Exec(ctx,
+		`INSERT INTO user_profile (user_id, fname, lname, address) VALUES($1,$2,$3,$4)`,
+		acct.ID, acct.Fname, acct.Lname, acct.Address,
+	)
+
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			fmt.Println(pgErr.Code)
+			fmt.Println(pgErr.Message)
+		}
+		return fmt.Errorf("error: %s, code: %s", pgErr.Message, pgErr.Code)
+	}
+	return nil
+}
+
+// add a user account and profile to the database
+func CreateAcct(acct Account) (string, error) {
+	err := addUserAcct(&acct)
+	if err != nil {
+		return "", fmt.Errorf("error: %w", err)
+	}
+
+	var userID int
+	userID, err = UserIDByUsername(acct.Username)
+	if err != nil {
+		return "", fmt.Errorf("error: %w", err)
+	}
+
+	err = addUserProfile(acct)
+	if err != nil {
+		return "", fmt.Errorf("error: %w", err)
+	}
+
+	return fmt.Sprintf("user %d has been registered", userID), nil
 }
