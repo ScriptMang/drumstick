20ebdf926b81f6476145d446586eb360019fed7b
Andy Peralta <aperalta686@gmail.com>
2025-02-05 15:38:15 -0500

    Rename struct fields with underscores in accts
    - renamed struct fields in UserProfile and Posts
      from using underscores
    
      - UserProfile
       - renamed "User_ID" to "UserID"
    
      - Posts
       - renamed "User_ID" to "UserID"
       - renamed "Number_Comments" to "NumbComments"
       - renamed "Number_Reposts" to "NumbReposts"
       - renamed "Number_Likes" to "NumbLikes"
       - renamed "Number_Views" to "NumbViews"
       - renamed "Number_Bookmarks" to "NumbBookMarks"

--- internal/accts/accts.go
+++ internal/accts/accts.go
@@ -2,7 +2,7 @@ package accts
 
 type UserProfile struct {
 	ID      int    `json:"id" form:"id"`
-	User_ID int    `json:"user_id" form:"user_id"`
+	UserID  int    `json:"user_id" form:"user_id"`
 	Fname   string `json:"fname" form:"fname"`
 	Lname   string `json:"lname" form:"lname"`
 	Address string `json:"address" form:"address"`
@@ -24,12 +24,14 @@ type Account struct {
 }
 
 type Posts struct {
-	ID               int    `json:"id" form:"id"`
-	User_ID          int    `json:"user_id" form:"user_id"`
-	Content          string `json:"content" form:"content"`
-	Number_Comments  int    `json:"number_comments" form:"number_comments"`
-	Number_Reposts   int    `json:"number_reposts" form:"number_reposts"`
-	Number_Likes     int    `json:"number_likes" form:"number_likes"`
-	Number_Views     int    `json:"number_views" form:"number_views"`
-	Number_Bookmarks int    `json:"number_bookmarks" form:"number_bookmarks"`
+	ID            int    `json:"id" form:"id"`
+	UserID        int    `json:"user_id" form:"user_id"`
+	Content       string `json:"content" form:"content"`
+	NumbComments  int    `json:"number_comments" form:"number_comments"`
+	NumbReposts   int    `json:"number_reposts" form:"number_reposts"`
+	NumbLikes     int    `json:"number_likes" form:"number_likes"`
+	NumbViews     int    `json:"number_views" form:"number_views"`
+	NumbBookmarks int    `json:"number_bookmarks" form:"number_bookmarks"`
+}
+
 }
