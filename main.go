31309c399f43df20257cc36dd3978fd7e627ec89
Andy Peralta <aperalta686@gmail.com>
2025-02-06 13:27:53 -0500

    Render the homepage using echo framework
    added the use of the echo framework along with template
    tmpl files to render and serve the homepage.
    
    - added TemplateManager struct to manage templates
     - added funct Render to execute the templates
    
    - added the use of echo framework in main
    
    - added the home.tmpl file
    
    - removed old welcome message "welcome to drumstick"

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"os"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
 
 	"scriptmang/drumstick/internal/accts"
-	"scriptmang/drumstick/internal/backend"
 
-	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/labstack/echo/v4"
 )
 
-func main() {
+type TemplateManager struct {
+	templates *template.Template
+}
+
+func (tm *TemplateManager) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+	err := tm.templates.ExecuteTemplate(w, name, data)
+
+	if err != nil {
+		log.Println("template not found")
+	}
+
+	return err
+}
+
+// func viewPosts(w http.ResponseWriter, r *http.Request) {
+// }
+
 
 	ctx, db := backend.CreatePool()
 	defer db.Close()
@@ -20,6 +42,20 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err.Error())
 		os.Exit(1)
+func homePage(c echo.Context) error {
+	data := "Welcome to drumstick"
+	return c.Render(http.StatusOK, "home", data)
+}
+
+// func login(w http.ResponseWriter, r *http.Request) {
+
+// }
+
+func main() {
+	tm := &TemplateManager{
+		templates: template.Must(template.ParseGlob("ui/html/pages/*[^#?!|].tmpl")),
 	}
-	fmt.Printf("welcome to Drumstick!, your user profile is %v\n", userProf)
+	router := echo.New()
+	router.Renderer = tm
+	router.GET("/", homePage)
 }
