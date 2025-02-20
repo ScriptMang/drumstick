78b19918b208e20eb1caccb4428438376f45dc95
Andy Peralta <aperalta686@gmail.com>
2025-02-20 10:15:34 -0500

    Start work on basic authentication
    started work on basic authentication to verfiy
    the username and password provided are legitimate
    
    - added func basicAuthValidator that takes a string
      email and password and compare them to those that
      exist in the database. its still a work in progress
       - the usercreds provided are placeholders

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/subtle"
 	"errors"
 	"fmt"
 	"html/template"
@@ -92,12 +93,21 @@ func vetLogin(c echo.Context) error {
 	return c.Render(http.StatusOK, "posts", "My Feed")
 }
 
+func basicAuthValidator(email, password string, c echo.Context) (bool, error) {
+	if subtle.ConstantTimeCompare([]byte(email), []byte("email")) == 1 &&
+		subtle.ConstantTimeCompare([]byte(password), "password") == 1 {
+		return true, nil
+	}
+	return false, nil
+}
+
 func main() {
 	tm := &TemplateManager{
 		templates: template.Must(template.ParseGlob("ui/html/pages/*[^#?!|].tmpl")),
 	}
 	router := echo.New()
 	router.Use(middleware.SecureWithConfig(middleware.DefaultSecureConfig))
+	router.Use(middleware.BasicAuth(basicAuthValidator))
 	router.Renderer = tm
 	router.GET("/", homePage)
 	router.GET("/signup", signUp)
