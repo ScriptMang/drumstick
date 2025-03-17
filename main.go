b3b6f64600d22b48c4ad3a40d5b03fdb226c66b9
Andy Peralta <aperalta686@gmail.com>
2025-03-17 16:28:01 -0400

    Rewrite the main app file w/ repsect to sessionmanager
    After separating the jwt logic into the sessionamanger pkg,
    functs and files that reference it needed to be updated.
    Some functionality, like the config of a jwt middleware
    had to be done in main.
    
    - changes in the main file
    
      - http handler vetLogin now calls sessionmanager
        to create a jwt token
    
     - added POST route "/refresh" to add new user posts
      - http handler addPosts calls sessionmanager
        to retrieve the user's email to go about
        creating a new post
    
      - added restricted handler for testing that
        user routes can't be accessed by anyone.
        it’s still a wip
    
      - sidenote: removed commented out viewPosts handler,
        since it wasn't being used
    
    - added refreshposts.tmpl, this page is meant
      to reload and show all the users posts. Along with
      the chance to upload another post.
      It's still a wip
    
    - changed post.tmpl, its form action now points to
      route "/refresh" instead of "/view"

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ import (
 
 	"scriptmang/drumstick/internal/accts"
 	"scriptmang/drumstick/internal/posts"
+	"scriptmang/drumstick/internal/sessionmanager"
 
+	"github.com/golang-jwt/jwt/v5"
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -35,9 +38,6 @@ func (tm *TemplateManager) Render(w io.Writer, name string, data interface{}, c 
 
 	return err
 }
-
-// func viewPosts(w http.ResponseWriter, r *http.Request) {
-// }
 
 func signUp(c echo.Context) error {
 	data := "Register a New User"
@@ -87,28 +87,81 @@ func vetLogin(c echo.Context) error {
 
 	rsltErr := accts.CompareUserCreds(usr, pswd)
 	if rsltErr != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, rsltErr)
+		return echo.NewHTTPError(http.StatusUnauthorized, rsltErr)
 	}
 
-	return c.Render(http.StatusOK, "posts", "My Feed")
+	t, err := sessionmanager.CreateToken(usr)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	return c.Render(http.StatusOK, "posts", echo.Map{
+		"token": t,
+	})
 }
 
-func addPosts(c echo.Context) {
-	mypost := c.FormValue("content")
-	posts.CreatePosts()
+// this funct is only  for testing purpsos
+func restricted(c echo.Context) error {
+	email, err := sessionmanager.Email(c)
+
+	// returns an http error on failure
+	if err != nil {
+		return err
+	}
+
+	return c.String(http.StatusOK, "Welcome "+email+"!")
+}
+
+func addPosts(c echo.Context) error {
+	myPost := c.FormValue("content")
+
+	email, retrievalErr := sessionmanager.Email(c)
+	if retrievalErr != nil {
+		return retrievalErr
+	}
+
+	newPost, err := posts.CreatePosts(myPost, email)
+
+	// handle errs where creating a post fails
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	// pass list of the user posts
+	// to their posts page
+
+	return c.Render(http.StatusOK, "refresh", *newPost[0])
 }
 
 func main() {
 	tm := &TemplateManager{
 		templates: template.Must(template.ParseGlob("ui/html/pages/*[^#?!|].tmpl")),
 	}
+
 	router := echo.New()
 	router.Use(middleware.SecureWithConfig(middleware.DefaultSecureConfig))
+
 	router.Renderer = tm
+
 	router.GET("/", homePage)
 	router.GET("/signup", signUp)
 	router.GET("/loginForm", loginForm)
 	router.POST("/view", accountCreation)
 	router.POST("/posts", vetLogin)
+	router.POST("/refresh", addPosts)
+
+	// Restricted group
+	r := router.Group("/restricted")
+
+	// Configure middleware with the custom claims type
+	config := echojwt.Config{
+		NewClaimsFunc: func(c echo.Context) jwt.Claims {
+			return new(sessionmanager.UserCustomClaims)
+		},
+		SigningKey: []byte("secret"),
+	}
+	r.Use(echojwt.WithConfig(config))
+	r.GET("", restricted)
+
 	router.Logger.Fatal(router.Start(":8080"))
 }
