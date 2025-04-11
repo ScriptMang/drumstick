9b6f6cab3c0a6842e58253f5cc71b2f696f2ad28
Andy Peralta <aperalta686@gmail.com>
2025-04-11 19:46:39 -0400

    Add the other get request routes for testing
    This will add the signup and loginform routes
    to main_test.
    
     - added func Test_Signup to test the signup rotue
     - added func Test_Loginpage to test the loginForm route

--- main_test.go
+++ main_test.go
@@ -24,6 +24,7 @@ func TestMain(m *testing.M) {
 	os.Exit(exitVal)
 }
 
+// test to see if the hompage route is acessible
 func Test_homepage(t *testing.T) {
 	// log.Print("Testing Homepage")
 	tm := &TemplateManager{
@@ -49,6 +50,64 @@ func Test_homepage(t *testing.T) {
 
 	c.SetPath("/")
 	if assert.NoError(t, homePage(c)) {
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.NotEqual(t, `{"message":"Not Found"}`, w.Body.String())
+	}
+}
+
+func Test_signup(t *testing.T) {
+	tm := &TemplateManager{
+		templates: template.Must(template.ParseGlob("ui/html/pages/*[^#?!|].tmpl")),
+	}
+
+	r := echo.New()
+
+	r.Use(middleware.SecureWithConfig(middleware.DefaultSecureConfig))
+	r.Use(middleware.Logger())
+	r.Use(middleware.Recover())
+
+	r.Renderer = tm
+
+	req, err := http.NewRequest("GET", "/signup", nil)
+
+	if err != nil {
+		t.Fatalf("error:\n %v\n", err)
+	}
+
+	w := httptest.NewRecorder()
+	c := r.NewContext(req, w)
+
+	c.SetPath("/signup")
+	if assert.NoError(t, signUp(c)) {
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.NotEqual(t, `{"message":"Not Found"}`, w.Body.String())
+	}
+}
+
+func Test_loginpage(t *testing.T) {
+	tm := &TemplateManager{
+		templates: template.Must(template.ParseGlob("ui/html/pages/*[^#?!|].tmpl")),
+	}
+
+	r := echo.New()
+
+	r.Use(middleware.SecureWithConfig(middleware.DefaultSecureConfig))
+	r.Use(middleware.Logger())
+	r.Use(middleware.Recover())
+
+	r.Renderer = tm
+
+	req, err := http.NewRequest("GET", "/loginForm", nil)
+
+	if err != nil {
+		t.Fatalf("error:\n %v\n", err)
+	}
+
+	w := httptest.NewRecorder()
+	c := r.NewContext(req, w)
+
+	c.SetPath("/loginForm")
+	if assert.NoError(t, loginForm(c)) {
 		assert.Equal(t, http.StatusOK, w.Code)
 		assert.NotEqual(t, `{"message":"Not Found"}`, w.Body.String())
 	}
