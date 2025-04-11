f0e65d7ab0d3984a0cb09e622d04d762a7cba17f
Andy Peralta <aperalta686@gmail.com>
2025-04-11 16:29:35 -0400

    Add unit testing for the main file
    Added unit testing to test all routes for edgecases.
    Currently there is only a unit test for the homepage
    route. The rest will follow
    
    - new file: main_test
     - added func TestMain to handle setup and cleanup of tests
     - added func Test_Homepage to test
       the homepage route

---
+++ main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/stretchr/testify/assert"
+)
+
+var testTimeStamp time.Time
+
+func TestMain(m *testing.M) {
+	log.Println("Setting up for tests")
+	testTimeStamp = time.Now()
+	exitVal := m.Run()
+	log.Println("Clean up after tests")
+	os.Exit(exitVal)
+}
+
+func Test_homepage(t *testing.T) {
+	// log.Print("Testing Homepage")
+	tm := &TemplateManager{
+		templates: template.Must(template.ParseGlob("ui/html/pages/*[^#?!|].tmpl")),
+	}
+
+	r := echo.New()
+
+	r.Use(middleware.SecureWithConfig(middleware.DefaultSecureConfig))
+	r.Use(middleware.Logger())
+	r.Use(middleware.Recover())
+
+	r.Renderer = tm
+
+	req, err := http.NewRequest("GET", "/", nil)
+
+	if err != nil {
+		t.Fatalf("error:\n %v\n", err)
+	}
+
+	w := httptest.NewRecorder()
+	c := r.NewContext(req, w)
+
+	c.SetPath("/")
+	if assert.NoError(t, homePage(c)) {
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.NotEqual(t, `{"message":"Not Found"}`, w.Body.String())
+	}
+}
