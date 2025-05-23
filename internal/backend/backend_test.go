89a78d8a57ff837afebec08a453947f2eb2da60e
Andy Peralta <aperalta686@gmail.com>
2025-05-23 15:59:40 -0400

    Implement ping test in test_connection funct
    Implemented a ping test to check for errors that can the connection to
    the database to fail for the Test_connection funct in backend_test file.

--- internal/backend/backend_test.go
+++ internal/backend/backend_test.go
@@ -5,5 +5,11 @@ import (
 )
 
 func Test_connection(t *testing.T) {
+	ctx, db := Connect()
+	defer db.Close()
 
+	failedDBConn := db.Ping(ctx)
+	if failedDBConn != nil {
+		t.Fatalf("server error: %s\n", failedDBConn.Error())
+	}
 }
