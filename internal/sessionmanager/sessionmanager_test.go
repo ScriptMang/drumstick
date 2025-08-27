967601f80d945f09fce3c893ae3b0e99c6469ce9
ScriptMang <33267648+ScriptMang@users.noreply.github.com>
2025-08-26 23:53:08 -0400

    Add invalid claims func to sessionmanager test file
    There are multiple reasons for an invalid claim in a jwt, so
    the invalid claims func was made to be able to catch them all.
    
    Changes made to the sessionmanager_test file
     - added the Test invalidClaimsType to check for invalid jwts

--- internal/sessionmanager/sessionmanager_test.go
+++ internal/sessionmanager/sessionmanager_test.go
@@ -2,9 +2,12 @@ package sessionmanager
 
 import (
 	"html/template"
+	"os"
 	"scriptmang/drumstick/internal/templateRenderer"
 	"testing"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -21,9 +24,26 @@ func setupEchoClient() *echo.Echo {
 	r.Use(middleware.Recover())
 
 	r.Renderer = tm
-
 	return r
 }
 
+func Test_invalidClaimsType(t *testing.T) {
+	secret := []byte(os.Getenv("HMAC_SECRET"))
+	expirDate := time.Now().Add(time.Hour * 72).Unix()
+	claims := &UserCustomClaims{
+		Email:      "dummy@gmail.com",
+		IsLoggedIn: true,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Unix(expirDate, 0)),
+		},
+	}
 
+	tokenStr, _ := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(secret)
+	token, err := jwt.ParseWithClaims(tokenStr, &UserCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return secret, nil
+	})
+
+	if err == nil && token.Valid {
+		t.Errorf("Expected claims type mismatch error")
+	}
 }
