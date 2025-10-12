46239e22da8aee94d3e67238029b0e010331df55
ScriptMang <33267648+ScriptMang@users.noreply.github.com>
2025-10-12 16:13:07 -0400

    Update echo client setup for account test file
    Update the template manager to include the template func dict
    in the accts_test file.

--- internal/accts/accts_test.go
+++ internal/accts/accts_test.go
@@ -17,9 +17,27 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+func dict(vals ...any) (map[string]any, error) {
+	if len(vals)%2 != 0 {
+		return nil, fmt.Errorf("dictionary requires an even number of arguments")
+	}
+
+	m := make(map[string]any, len(vals)/2)
+	for i := 0; i < len(vals); i += 2 {
+		ky, ok := vals[i].(string)
+		if !ok {
+			return nil, fmt.Errorf("dictionary keys must be string type")
+		}
+		m[ky] = vals[i+1]
+	}
+	return m, nil
+}
+
 func setupEchoClient() *echo.Echo {
 	tm := &templateRenderer.TemplateManager{
-		Templates: template.Must(template.ParseGlob("../../ui/html/pages/*[^#?!|].tmpl")),
+		Templates: template.Must(template.New("").Funcs(template.FuncMap{
+			"dict": dict,
+		}).ParseGlob("../../ui/html/pages/*[^#?!|].tmpl")),
 	}
 
 	r := echo.New()
