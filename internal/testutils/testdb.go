2fb5bed15446992fa62303f0821ff95059c02af8
ScriptMang <33267648+ScriptMang@users.noreply.github.com>
2025-10-14 16:18:57 -0400

    Add func testpool to connect to the test database
    Implemented the helper func TestPool which connects to
    the test database.
    
    Changes made to testdb.go
     - new helper func TestPool, it connects to the test database

--- internal/testutils/testdb.go
+++ internal/testutils/testdb.go
@@ -1,1 +1,29 @@
 package testutils
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func TestPool(t *testing.T) *pgxpool.Pool {
+	t.Helper()
+	dbURL := os.Getenv("TEST_DATABASE_URL")
+	if dbURL == "" {
+		t.Fatal("env var TEST_DATABASE_URL  is not set")
+	}
+
+	ctx := context.Background()
+	pool, err := pgxpool.New(ctx, dbURL)
+	if err != nil {
+		t.Fatalf("failed to connect to database: %v", err)
+	}
+
+	t.Cleanup(func() {
+		pool.Close()
+	})
+
+	return pool
+}
