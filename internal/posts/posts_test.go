20c1cb4380c50a22520c9276e38016cfd9e895e6
ScriptMang <33267648+ScriptMang@users.noreply.github.com>
2025-11-08 14:31:00 -0500

    Remove call to setup echo-client, dict for posts_test file
    After updating the unit-tests the func setupEchoClient and its helper
    func dict aren't needed anymore. For that reason they'll be removed.
    
    changes to posts_test file
     - removed func dict
     - removed func setupEchoClient

--- internal/posts/posts_test.go
+++ internal/posts/posts_test.go
@@ -4,50 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"html/template"
 	"scriptmang/drumstick/internal/accts"
-	"scriptmang/drumstick/internal/templateRenderer"
 	"scriptmang/drumstick/internal/testutils"
 	"testing"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
-
-func dict(vals ...any) (map[string]any, error) {
-	if len(vals)%2 != 0 {
-		return nil, fmt.Errorf("dictionary requires an even number of arguments")
-	}
-
-	m := make(map[string]any, len(vals)/2)
-	for i := 0; i < len(vals); i += 2 {
-		ky, ok := vals[i].(string)
-		if !ok {
-			return nil, fmt.Errorf("dictionary keys must be string type")
-		}
-		m[ky] = vals[i+1]
-	}
-	return m, nil
-}
-
-func setupEchoClient() *echo.Echo {
-	tm := &templateRenderer.TemplateManager{
-		Templates: template.Must(template.New("").Funcs(template.FuncMap{
-			"dict": dict,
-		}).ParseGlob("../../ui/html/pages/*[^#?!|].tmpl")),
-	}
-
-	r := echo.New()
-
-	r.Use(middleware.SecureWithConfig(middleware.DefaultSecureConfig))
-	r.Use(middleware.Logger())
-	r.Use(middleware.Recover())
-
-	r.Renderer = tm
-
-	return r
-}
 
 // tests the func that returns the username of a post given the account id
 func Test_UsernameByID(t *testing.T) {
