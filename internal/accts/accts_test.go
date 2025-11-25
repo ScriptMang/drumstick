128f11b89ffc0632333c809dd6e1215479e1147d
ScriptMang <33267648+ScriptMang@users.noreply.github.com>
2025-11-25 17:31:49 -0500

    Import accts errs for account creation unit-test (refs c985fea)
    After making all the errors global in previous commit,
    I replaced the duplicates errs with the real ones
    in the unit-test func Test_accountcreation.

--- internal/accts/accts_test.go
+++ internal/accts/accts_test.go
@@ -69,43 +69,25 @@ func bind_TestCase(t *testing.T, dir string, file string) create_acct_testcase {
 }
 
 func Test_accountcreation(t *testing.T) {
-	// field errors
-	errEmptyField := errors.New("field is empty")
-	errHasNums := errors.New("field can't contain any numbers")
-	errHasSymbols := errors.New("field can't contain any symbols")
-	errHasPunct := errors.New("field has punctuation")
-
-	// email errs
-	errReqSymbol := errors.New("email is missing an '@' symbol.")
-	errReqEndingAddr := errors.New("email doesn't match any of the ending addresses.")
-
-	// password errs
-	emptyPswd := errors.New("field can't be empty")
-	shortPswd := errors.New("field too short")
-	longPswd := errors.New("field too long")
-	pswdHasNoDigits := errors.New("field missing at least 1 digit")
-	pswdHasSymbols := errors.New("field can't contain any symbols")
-	missingCapitalLetter := errors.New("field is missing at least 1 capital letter")
-
 	testCaseLst := []testcase{
-		{"case01_fname_lname_empty.json", []error{errEmptyField, errEmptyField}},
-		{"case02_fname_has_spaces.json", []error{errHasPunct}},
-		{"case03_fname_has_nums.json", []error{errHasNums}},
-		{"case04_fname_has_symbols.json", []error{errHasSymbols}},
-		{"case05_lname_has_spaces.json", []error{errHasPunct}},
-		{"case06_lname_has_nums.json", []error{errHasNums}},
-		{"case07_lname_has_symbols.json", []error{errHasSymbols}},
-		{"case08_addr_empty.json", []error{errEmptyField}},
-		{"case09_addr_has_symbols.json", []error{errHasSymbols}},
-		{"case10_email_empty.json", []error{errEmptyField}},
-		{"case11_email_bad_addr.json", []error{errReqEndingAddr}},
-		{"case12_email_miss_symbol.json", []error{errReqSymbol}},
-		{"case13_pswd_empty.json", []error{emptyPswd}},
-		{"case14_pswd_miss_capital.json", []error{missingCapitalLetter}},
-		{"case15_pswd_too_short.json", []error{shortPswd, missingCapitalLetter, pswdHasNoDigits}},
-		{"case16_pswd_too_long.json", []error{longPswd}},
-		{"case17_pswd_miss_digits.json", []error{missingCapitalLetter, pswdHasNoDigits}},
-		{"case18_pswd_has_symbols.json", []error{pswdHasSymbols}},
+		{"case01_fname_lname_empty.json", []error{ErrEmptyField, ErrEmptyField}},
+		{"case02_fname_has_spaces.json", []error{ErrHasPunct}},
+		{"case03_fname_has_nums.json", []error{ErrHasNums}},
+		{"case04_fname_has_symbols.json", []error{ErrHasSymbols}},
+		{"case05_lname_has_spaces.json", []error{ErrHasPunct}},
+		{"case06_lname_has_nums.json", []error{ErrHasNums}},
+		{"case07_lname_has_symbols.json", []error{ErrHasSymbols}},
+		{"case08_addr_empty.json", []error{ErrEmptyField}},
+		{"case09_addr_has_symbols.json", []error{ErrHasSymbols}},
+		{"case10_email_empty.json", []error{ErrEmptyField}},
+		{"case11_email_bad_addr.json", []error{ErrReqEndingAddr}},
+		{"case12_email_miss_symbol.json", []error{ErrReqEmailSymbol}},
+		{"case13_pswd_empty.json", []error{EmptyPswd}},
+		{"case14_pswd_miss_capital.json", []error{PswdHasNoCapLetters}},
+		{"case15_pswd_too_short.json", []error{PswdTooShort, PswdHasNoCapLetters, PswdHasNoDigits}},
+		{"case16_pswd_too_long.json", []error{PswdTooLong}},
+		{"case17_pswd_miss_digits.json", []error{PswdHasNoCapLetters, PswdHasNoDigits}},
+		{"case18_pswd_has_symbols.json", []error{PswdHasSymbols}},
 	}
 	tests := []create_acct_testcase{}
 	for _, tc := range testCaseLst {
@@ -121,7 +103,6 @@ func Test_accountcreation(t *testing.T) {
 			actualErrLst := VetAllFields(tempAcct)
 			expectedErrLst := printAcctErrs(tt.AttribNames, tt.ErrLst)
 			assert.Equal(t, expectedErrLst, actualErrLst, expectedErrLst)
-
 		})
 	}
 }
