c71d0f54c81169ac2aba9b505c2d7bd5edf1f485
ScriptMang <33267648+ScriptMang@users.noreply.github.com>
2025-11-25 16:21:48 -0500

    Remove setupechoclient and dict func  in accts_tests file
    The setupEchoClient func and its helper func dict were being anymore
    by any of the unit-test. All the tests were rewritten, leaving those
    funcs to not be used anymore.
    
    Changes to accts_tests file
    - rmv funcs that weren't being used by any of the unit-tests
     - func setupEchoClient
     - func dict

--- internal/accts/accts_test.go
+++ internal/accts/accts_test.go
@@ -5,17 +5,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"html/template"
 	"os"
 	"path/filepath"
 	"testing"
 
-	"scriptmang/drumstick/internal/templateRenderer"
 	"scriptmang/drumstick/internal/testutils"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -31,40 +27,6 @@ type create_acct_testcase struct {
 type testcase struct {
 	path   string
 	errLst []error
-}
-
-func dict(vals ...any) (map[string]any, error) {
-	if len(vals)%2 != 0 {
-		return nil, fmt.Errorf("dictionary requires an even number of arguments")
-	}
-
-	m := make(map[string]any, len(vals)/2)
-	for i := 0; i < len(vals); i += 2 {
-		ky, ok := vals[i].(string)
-		if !ok {
-			return nil, fmt.Errorf("dictionary keys must be string type")
-		}
-		m[ky] = vals[i+1]
-	}
-	return m, nil
-}
-
-func setupEchoClient() *echo.Echo {
-	tm := &templateRenderer.TemplateManager{
-		Templates: template.Must(template.New("").Funcs(template.FuncMap{
-			"dict": dict,
-		}).ParseGlob("../../ui/html/pages/*[^#?!|].tmpl")),
-	}
-
-	r := echo.New()
-
-	r.Use(middleware.SecureWithConfig(middleware.DefaultSecureConfig))
-	r.Use(middleware.Logger())
-	r.Use(middleware.Recover())
-
-	r.Renderer = tm
-
-	return r
 }
 
 func printAcctErrs(attribNames []string, errMsgs []error) []error {
